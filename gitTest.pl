b6bf0131edfd39ca710804297e5bf444909059b8
degan85 <degan8535@gmail.com>
2020-06-23 10:50:17 +0900

    first commit

---
+++ gitTest.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: gitTest.pl
+#
+#        USAGE: ./gitTest.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 06/23/2020 10:46:48 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
+test Master;
